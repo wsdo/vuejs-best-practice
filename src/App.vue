428492ccebad5110117d9f152c16bd9aa32bd9e3
starkwang <wsd312@163.com>
2019-05-31 17:08:40 +0800

    解决冲突

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    sasasassa211
+    sasasassa211asdasa
     <Stark msg="Welcome to Your Vue.js App" />
   </div>
 </template>
