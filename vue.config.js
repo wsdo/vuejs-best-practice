a18428556a3c181c405fc0d1e43b56596562b13e
starkwang <wsd312@163.com>
2019-06-02 21:01:43 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,8 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  // lintOnSave: false,
+  lintOnSave: process.env.NODE_ENV !== 'production',
   css: {
     modules: true,
     loaderOptions: {
@@ -28,6 +30,39 @@ module.exports = {
     }
   },
   chainWebpack: (config) => {
+    config.optimization.minimize(true);
+    // config.optimization.splitChunks({
+    //   maxSize: 500,
+    //   chunks: 'all'
+    // })
+    config.optimization.splitChunks({
+            // chunks: 'async',
+      // minSize: 300,
+
+      // minChunks: 1,
+      cacheGroups: {
+        vendors: {
+          name: 'chunkstark-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          chunks: 'initial'
+        },
+        common: {
+          name: 'chunk-common',
+          minChunks: 2,
+          maxSize: 100,
+          priority: -20,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        }
+      }
+    })
+    // config.externals({
+    //   vue: 'vue',
+    //   vuex: 'vuex',
+    //   'vue-router': "'vue-router'",
+    //   'element-ui': "'element-ui'"
+    // })
     config.resolve.alias
       .set('@', resolve('src'))
       .set('assets', resolve('src/assets'))
